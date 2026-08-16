8f940e7efbf9c9a75889d2bf4ecfd43f83954f1c
Thomas (Yanle) Li <lyole20@gmail.com>
2026-08-15 23:26:12 -0400

    prepare test of pipeline

---
+++ analysis_script/select_test_exons.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+
+df = pd.read_csv(ROOT / "arush_data" / "exonic_variants.csv")
+
+selected_exons = [
+    "ATM_e56", #stubborn
+    "BLM_e17", #stubborn
+    "PHYHD1_e11",
+    "NPC1L1_e7",
+    "GDPD2_e2",
+    "ADGRG3_e3",
+    "ABLIM3_e14" #2 clusters
+    "TRPM6_e23",
+    "SNRNP70_e8",
+    "SCN5A_e6",
+    "SCN8A_e6"
+]
+
+df = df[df["exon_id"].isin(selected_exons)]
+df = df.dropna(subset=['psi', 'logit_psi']) #has to have values
+
+df.to_csv(ROOT / "outputs" / "test_exons.csv", index=False)
